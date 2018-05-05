31cc4100b7ef15c6d6be51e45aef66e87de9d9db
Tjitsen <r0709243@student.thomasmore.be>
2018-05-05 16:59:46 +0200

    images

---
+++ public_html/mail/contact_me.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html lang="nl">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Intentio is een dynamisch team dat gemotiveerd is om uw onderneming sterker te maken met een mooi-ogende en functionele website.
+    Wij maken elke website speciaal voor u op maat zodat al uw noden vervuld kunnen worden.">
+    <meta name="author" content="Intentio">
+
+    <title>Intentio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/freelancer.min.css">
+
+    <!-- Custom fonts for this template -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+
+    <!-- shortcut icon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+    <link rel="manifest" href="../site.webmanifest">
+    <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#00bfff">
+    <meta name="msapplication-TileColor" content="#89bdd3">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Plugin CSS -->
+    <link href="../js/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/freelancer.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top" style="font-family: 'Hind', sans-serif">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav" style="font-family: sans-serif">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="../">Intentio</a>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../index.html#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../index.html#about">Over ons</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../index.html#team">Team</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="../index.html#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+	<section id="contact" style="margin-top:100px!important;">
+		<div class="container">
+			<h2 class="text-center text-uppercase text-secondary mb-0">Bericht verzonden!</h2>
+        		<hr class="star-dark mb-5">
+			<p class="lead">
+				Bedankt voor uw inzending. Wij nemen spoedig contact met u op.<br>U gaf ons volgende gegevens door:<br><br>			</p>
+			<p class="lead">
+				<span style='color: black; background: #FFECEC; border: 1px solid red; border-radius: 3px;'><br />
+<b>Notice</b>:  Undefined index: name in <b>/home/users/w/woutcollart/public_html/bootstrap/mail/contact_me.php</b> on line <b>80</b><br />
+</span><span style='font-weight:bold;'>Naam: </span><br><span style='color: black; background: #FFECEC; border: 1px solid red; border-radius: 3px;'><br />
+<b>Notice</b>:  Undefined index: email in <b>/home/users/w/woutcollart/public_html/bootstrap/mail/contact_me.php</b> on line <b>81</b><br />
+</span><span style='font-weight:bold;'>Email: </span><br><span style='color: black; background: #FFECEC; border: 1px solid red; border-radius: 3px;'><br />
+<b>Notice</b>:  Undefined index: message in <b>/home/users/w/woutcollart/public_html/bootstrap/mail/contact_me.php</b> on line <b>82</b><br />
+</span><span style='font-weight:bold;'>Bericht: </span><br>			</p>
+		</div>
+	</section>
+</body>
+</html>
+
